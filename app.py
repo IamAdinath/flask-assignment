12a53f20017c0cd65c689a586fc0cbaf3f690031
Adinath Gore <59023488+IamAdinath@users.noreply.github.com>
2023-10-04 02:14:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config['INFLUXDB_PORT'] = 8086  # Replace with your InfluxDB port
 app.config['INFLUXDB_DATABASE'] = 'bulk_data'
 app.config['INFLUXDB_USERNAME'] = 'admin'
 app.config['INFLUXDB_PASSWORD'] = 'password'
+app.config['host'] = '0.0.0.0'
 
 # Register the data blueprint with "/api" as the URL prefix
 app.register_blueprint(api_blueprint, url_prefix='/api')
