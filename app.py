213346ee67c091dcd62862ce00df68217837bd07
IamAdinath <adinathgore.17@gmail.com>
2023-10-04 01:27:36 +0530

    update the values and

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from data_api import api_blueprint  # Import the data blueprint
+from data_api import api_blueprint
 
-# Create the Flask app
 app = Flask(__name__)
-
 # Configuration for InfluxDB (replace with your own values)
 app.config['INFLUXDB_HOST'] = 'localhost'
 app.config['INFLUXDB_PORT'] = 8086  # Replace with your InfluxDB port
 app.config['INFLUXDB_DATABASE'] = 'bulk_data'
 app.config['INFLUXDB_USERNAME'] = 'admin'
-app.config['INFLUXDB_PASSWORD'] = 'admin'
+app.config['INFLUXDB_PASSWORD'] = 'password'
 
 # Register the data blueprint with "/api" as the URL prefix
 app.register_blueprint(api_blueprint, url_prefix='/api')
--- data_api.py
+++ data_api.py
@@ -1,36 +1,64 @@
-# data_api.py
 
-# Import necessary dependencies
-from flask import Blueprint, jsonify, current_app
+from flask import Blueprint, jsonify, current_app, request
 from influxdb import InfluxDBClient
-
+from datetime import datetime
 # Create a Blueprint object for the data module
 api_blueprint = Blueprint('data', __name__)
 
 @api_blueprint.route('/bulk-data', methods=['GET'])
 def get_bulk_data():
+    """
+    Fetch the data from  table and return as a response in get method.
+    """
     try:
-        # Connect to the InfluxDB database (replace with your own database configuration)
-        influx_client = InfluxDBClient(
-            username=current_app.config['INFLUXDB_USERNAME'],
-            password=current_app.config['INFLUXDB_PASSWORD'],
-            host=current_app.config['INFLUXDB_HOST'],
-            port=current_app.config['INFLUXDB_PORT'],
-            database=current_app.config['INFLUXDB_DATABASE']
-        )
-
-        # Query the database to retrieve bulk data (replace with your own query)
-        query = 'SELECT * FROM your_measurement_name LIMIT 100'
-        result = influx_client.query(query)
-
-        # Process the query result as needed (e.g., converting to a list of dictionaries)
+        influx_client = create_client(current_app)
+        result = influx_client.query('SELECT * FROM test_measurement2 LIMIT 100')
         data = list(result.get_points())
-
-        # Close the InfluxDB connection
         influx_client.close()
-
-        # Return the data as a JSON response
-        return jsonify({'data': data})
+        return jsonify(data)
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
+
+@api_blueprint.route('/post-data', methods=['GET'])
+def post_data():
+    try:
+        influx_client = create_client(current_app)
+        # data = request.get_json()  # Assuming data is posted as JSON
+
+        # Prepare data points
+        data_points = [
+            {
+                "measurement": "test_measurement2",
+                "tags": {
+                    "Job Profile": "Backend Developer",
+                },
+                "Application Timestamp": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+                "fields": {
+                    "Name": "John Doe",
+                    "Experience": "3",
+                    "Skills": "Python, Java, C#",
+                    "Role": "Software Engineer",
+                    "Salary": "100000",
+                    "Location": "New York, USA",
+                    "email": "XXXXXXXXXXXXXXXXXXX"
+                }
+            }
+        ]
+
+        influx_client.write_points(data_points)
+        influx_client.close()
+        return "Data posted successfully."
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+def create_client(current_app):
+    influx_client = InfluxDBClient(
+        username=current_app.config['INFLUXDB_USERNAME'],
+        password=current_app.config['INFLUXDB_PASSWORD'],
+        host=current_app.config['INFLUXDB_HOST'],
+        port=current_app.config['INFLUXDB_PORT'],
+        database=current_app.config['INFLUXDB_DATABASE']
+    )
+    return influx_client
